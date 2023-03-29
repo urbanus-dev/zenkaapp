ab654faca359673f2795e436c17aa15f4fe0b6d6
erastus munyao kyalo <erastusmunya2022@gmail.com>
2023-03-29 18:18:37 +0300

    made changes on register and created welcome.dart

---
+++ lib/Welcome.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container()
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
